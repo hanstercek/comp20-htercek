1efd119fcc523524f52cc833544643267ac901ef
Hans Tercek <hanstercek@MacBook-Pro.local>
2017-06-19 10:40:05 -0400

    External JSON parse for messages

---
+++ messages/README.md
@@ -0,0 +1,20 @@
+# messages pt 2 README
+# By: htercek
+# On: 6/18/17
+# Tufts COMP-20 Lab 9
+# Part 2:
+#       - It did not work as it is impossible to read local files using
+#         XMLHttpRequest due to security issues
+# Part 3:
+#       - It did work. It is possible to access external files if the
+#         site has been configured to support CORS
+# Implemented:
+#       Readme
+#       JSON parsing
+#       Javascript
+# Unimplemented:
+#
+# Collaborators:
+#
+# Hours Logged:
+#       2.5
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
 // Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+
+	request.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			elements = JSON.parse(request.responseText);
+			output = "";
+			for (i = 0; i < elements.length; i++) {
+				output += "<div class='newMessage'><p>" + elements[i]["content"] + " by <span class='author'>" + elements[i].username + "</span></p></div>";
+			}
+			document.getElementById("messages").innerHTML = output;
+		}
+	};
+
+	request.send();
+}
